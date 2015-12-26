1873a36836b4136ce83288878167b99123e0ab8a
someone755 <jari.begus@gmail.com>
2015-12-26 16:31:25 +0100

    kerneller: update to latest version
    Due to the (arguably more useful)  mkbootimg_tools branch, this one has been neglected. It's high time I start updating this first, and then update the branch I personally use.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 
 Inspired by AnyKernel, this tool allows Xperia kernels to be flashed to any ROM, regardless of the ramdisk used. This dramatically increases compatibility while decreasing size.
 
-But, unlike AnyKernel, this kernel installer allows for the SELinux setting to be set at **the time of install**, based on the user's choice. For kernel developers this means only one zip has to be uploaded, instead of both a permissive and an enforcing variant.
+But, unlike AnyKernel, this kernel installer allows for the SELinux setting to be set at **the time of install**, based on the user's choice. For kernel developers this means only one zip has to be uploaded, instead of both a permissive and an enforcing variant. This also reduces user confusion, storage usage, and server load.
 
-At this time, the tool is still fairly new and unpolished. Code is dirty, and compatibility with devices is severely limited, so pull requests and tips of any kind are appreciated.
+Although thoroughly tested on the Z3 and Z3C using TWRP, the tool still requires some testing on other devices and recoveries. Pull requests are appreciated.
+
+I'll leave the packing up to you. Though you're more than welcome to use the mkbootimg_tools branch to easily pack things up.
--- install.sh
+++ install.sh
@@ -1,73 +1,107 @@
-#!/sbin/busybox sh
+#!/sbin/sh
+# Define commonly used/long directory names.
+boot="/dev/block/platform/msm_sdcc.1/by-name/boot"
+tools="/tmp/kerneller/tools"
+work="/tmp/kerneller/work"
+OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
+ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
 # Remove files from previous installation -- if the user flashes the zip
 # twice, there would otherwise be issues, creating a useless boot.img
-rm -rf /tmp/kernel12
+rm -rf $work;
 # Use keycheck to determine what kind of image the user wants. Give it 30 seconds.
 # Vol+ means permissive, Vol- or no keypress means enforcing.
 choose () {
-	busybox chmod 777 /tmp/kernel12/tools/keycheck
-	busybox timeout -t 30 /tmp/kernel12/tools/keycheck
+	ui_print "You now have 30 seconds to press vol+/-";
+	ui_print " ";
+	ui_print "Pressing volume up will set SELinux to permissive.";
+	ui_print "Pressing volume down or not pressing anything will set it to enforcing.";
+	ui_print "Your move.";
+	ui_print " ";
+	chmod 777 $tools/keycheck;
+	timeout -t 30 $tools/keycheck;
 	if [[ $? -eq 42 ]]; then
-		mode="permissive"
+		ui_print "Making permissive image";
+		mode="permissive";
 	else
-		busybox echo "shitshitshit"
-		mode="enforcing"
+		ui_print "Making enforcing image";
+		mode="enforcing";
 	fi
 }
-
 # Extract the ramdisk, replace the crucial files, then compress it again.
 ramdisk () {
-	busybox dd if=/dev/block/mmcblk0p14 of=/tmp/kernel12/original.img
-	busybox chmod 777 /tmp/kernel12/tools/unpackbootimg
-	busybox mkdir /tmp/kernel12/work
-	/tmp/kernel12/tools/unpackbootimg -i /tmp/kernel12/original.img -o /tmp/kernel12/work
-	busybox mkdir /tmp/kernel12/work/combinedroot
-	cd /tmp/kernel12/work/combinedroot
-	busybox cat /tmp/kernel12/work/original.img-ramdisk.gz | busybox gzip -d | cpio -i -d
-	busybox mkdir /tmp/kernel12/work/ramdisk
-	cd /tmp/kernel12/work/ramdisk
-	busybox cat /tmp/kernel12/work/combinedroot/sbin/ramdisk.cpio | cpio -i -d
-	busybox cp /tmp/kernel12/res/fstab.qcom /tmp/kernel12/work/ramdisk/fstab.qcom
-	busybox chmod 777 /tmp/kernel12/work/ramdisk/fstab.qcom
-	busybox cp /tmp/kernel12/res/init.sh /tmp/kernel12/work/combinedroot/sbin/init.sh
-	busybox chmod 777 /tmp/kernel12/work/combinedroot/sbin/init.sh
-	busybox find . | cpio -o -H newc > /tmp/kernel12/work/combinedroot/sbin/ramdisk.cpio
-	cd /tmp/kernel12/work/combinedroot
-	busybox find . | cpio -o -H newc | gzip -c > /tmp/kernel12/work/original.img-ramdisk.gz
-	cd /tmp/kernel12/work
+	ui_print "Modifying ramdisk";
+	dd if=$boot of=/tmp/kerneller/original.img;
+	chmod 777 $tools/unpackbootimg;
+	mkdir $work;
+	$tools/unpackbootimg -i /tmp/kerneller/original.img -o $work;
+	mkdir $work/combinedroot;
+	cd $work/combinedroot;
+	cat $work/original.img-ramdisk.gz | gzip -d | cpio -i -d;
+	mkdir $work/ramdisk;
+	cd $work/ramdisk;
+	cat $work/combinedroot/sbin/ramdisk.cpio | cpio -i -d;
+# At this point the ramdisk is completely extracted: begin making changes (copy and chmod)
+	cp /tmp/kerneller/res/fstab.qcom $work/ramdisk/fstab.qcom;
+	cp /tmp/kerneller/res/init.sh $work/combinedroot/sbin/init.sh;
+	chmod 777 $work/ramdisk/fstab.qcom;
+	chmod 777 $work/combinedroot/sbin/init.sh;
+# Changes that are not related to the ramdisk and still need to be reviewed:
+	# rm -rf /system/bin/mpdecision;
+	# rm -rf /system/bin/thermanager;
+# Repack the ramdisk back completely
+	find . | cpio -o -H newc > $work/combinedroot/sbin/ramdisk.cpio;
+	cd $work/combinedroot;
+	find . | cpio -o -H newc | gzip -c > $work/original.img-ramdisk.gz;
 }
 
 cmdline () {
-	# cd /tmp/kernel12/work
+	cd $work
 	# If Vol+ was pressed, check if cmdline already has the permissive tag
-	# If not, echo it in
+	# If yes, move on. If not, echo it in.
 	if [ $mode = "permissive" ]; then
-		if busybox cat /tmp/kernel12/work/original.img-cmdline | busybox grep androidboot.selinux=permissive; then
+		if cat $work/original.img-cmdline | grep androidboot.selinux=permissive; then
 			:
-		elif ! busybox cat /tmp/kernel12/work/original.img-cmdline | busybox grep androidboot.selinux=permissive; then
-			busybox echo "$(busybox cat /tmp/kernel12/work/original.img-cmdline) androidboot.selinux=permissive" >/tmp/kernel12/work/original.img-cmdline
+		elif ! cat $work/original.img-cmdline | grep androidboot.selinux=permissive; then
+			 echo "$(cat $work/original.img-cmdline) androidboot.selinux=permissive" >$work/original.img-cmdline
 		fi
 	# Else, check the cmdline and remove the tag if it's already present
 	else
-		if ! busybox cat /tmp/kernel12/work/original.img-cmdline | busybox grep androidboot.selinux=permissive; then
+		if ! cat $work/original.img-cmdline | grep androidboot.selinux=permissive; then
 			:
-		elif busybox cat /tmp/kernel12/work/original.img-cmdline | busybox grep androidboot.selinux=permissive; then
+		elif cat $work/original.img-cmdline | grep androidboot.selinux=permissive; then
 			# It's dirty, I know. I'm new to this.
 			# Tips and/or pull requests appreciated!
-			busybox cat /tmp/kernel12/work/original.img-cmdline | busybox sed 's/ androidboot.selinux=permissive//' >tmp
-			busybox cat tmp >/tmp/kernel12/work/original.img-cmdline
+			cat $work/original.img-cmdline | sed 's/ androidboot.selinux=permissive//' >tmp
+			cat tmp >$work/original.img-cmdline
 		fi
 	fi
-	busybox cat /tmp/kernel12/work/original.img-cmdline
+	cat $work/original.img-cmdline
 }
 
 mkimg () {
-	cd /tmp/kernel12
-	busybox chmod 777 /tmp/kernel12/tools/mkbootimg
-	/tmp/kernel12/tools/mkbootimg --kernel /tmp/kernel12/res/zImage --ramdisk /tmp/kernel12/work/original.img-ramdisk.gz --cmdline "$(busybox cat work/original.img-cmdline)" --board "$(busybox cat work/original.img-board)" --base "$(busybox cat work/original.img-base)" --pagesize "$(busybox cat work/original.img-pagesize)" --kernel_offset "$(busybox cat work/original.img-kerneloff)" --ramdisk_offset "$(busybox cat work/original.img-ramdiskoff)" --tags_offset "$(busybox cat work/original.img-tagsoff)" --dt /tmp/kernel12/res/dt.img -o /tmp/kernel12/boot.img
+	chmod 777 $tools/mkbootimg
+	$tools/mkbootimg --kernel /tmp/kerneller/res/zImage --ramdisk $work/original.img-ramdisk.gz --cmdline "$(cat $work/original.img-cmdline)" --board "$(cat $work/original.img-board)" --base "$(cat $work/original.img-base)" --pagesize "$(cat $work/original.img-pagesize)" --kernel_offset "$(cat $work/original.img-kerneloff)" --ramdisk_offset "$(cat $work/original.img-ramdiskoff)" --tags_offset "$(cat $work/original.img-tagsoff)" --dt /tmp/kerneller/res/dt.img -o /tmp/kerneller/boot.img
 }
 
+modcpy () {
+	cp -f /tmp/kerneller/modules/* /system/lib/modules/
+}
+
+# Functions are all set: Run them in order
 choose
 ramdisk
 cmdline
 mkimg
+# Check for one of the files we copied: if it's there, the boot
+# image was repacked succesfully. If not, flashing it would not
+# allow the device to boot.
+if [ -f $work/ramdisk/fstab.qcom ]; then
+  ui_print "Done messing around!";
+  ui_print "Writing the new boot.img...";
+  dd if=/tmp/kerneller/boot.img of=/dev/block/platform/msm_sdcc.1/by-name/boot
+  ui_print "Copying modules...";
+  modcpy
+else
+  ui_print "Error creating working boot image, aborting install!";
+  ui_print "Are you running a compatible recovery?";
+fi
